b947f6319a62a691af9b8454a41d01504220c44e
alexandra.kerezsi.18@gmail.com <alexandra.kerezsi.18@gmail.com>
2022-11-27 23:00:54 +0200

    Add project files.

---
+++ Lab1ex10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Lab1ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* Scrieti un program care sa va afisa suma cifrelor unui numar intreg citit de la tastatura. */
+
+            Console.WriteLine("Introduceti x");
+            int x = int.Parse(Console.ReadLine());
+
+            int ultimaCifra = x % 10;
+            int primaCifra = x / 10;
+            int sumaCifrelor = ultimaCifra + primaCifra;
+            Console.WriteLine("Suma cifrelor este: " + sumaCifrelor);
+
+        }
+    }
+}
